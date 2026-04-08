b442e231783253bb1c6d9253cb698c20053cac22
Rosewal Almeida <ros123almeida@gmail.com>
2026-04-08 16:20:52 +0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import pickle
+import numpy as np
+
+#Load the mmodels
+model = pickle.load(open('model.pkl','rb'))
+scaler = pickle.load(open('scaler.pkl','rb'))
+
+#streamlit UI
+st.title("Diabetes Prediction App")
+
+#User Input fields
+pregnancies = st.number_input("Pregnancies", min_value=0, max_value=20, value=0)
+glucose = st.number_input("Glucose Level", min_value = 0, max_value = 300, value = 120)
+blood_pressure = st.number_input("Blood Pressure", min_value=0, max_value=200, value=80)
+skin_thickness = st.number_input ("Skin Thickness", min_value=0, max_value = 100, value =20)
+insulin = st.number_input("Insulin Level", min_value=0, max_value=900, value=80)
+bmi = st.number_input("BMI", min_value=0.0, max_value=70.0, value=25.0, format="%.1f")
+dpf = st.number_input("Diabetes Pedigree Function", min_value=0.0, max_value=3.0, value=0.5, format="%.1f")
+age = st.number_input("Age", min_value=0, max_value=120, value=30)
+
+#Button to make predictions
+if st.button("Predict Diabetes"):
+    #Create Input Array
+    input_data = np.array([[pregnancies, glucose, blood_pressure, skin_thickness, insulin, bmi, dpf, age]])
+    
+    #Scale the input 
+    input_data_sacled = scaler.transform(input_data)
+
+    #Make predictions
+    prediction = model.predict(input_data_sacled)
+
+    #show_result
+    if prediction[0] == 1:
+        st.error ("The model predicts **Diabetes**.")
+    else:
+        st.success("The model predicts **No Diabetes**")
+
+    
